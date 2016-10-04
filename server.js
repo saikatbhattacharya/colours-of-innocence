23db033ab682122b3affe3bfb7a41565544a91a7
saikatcts <Saikat.Bhattacharya5@cognizant.com>
2016-10-05 00:27:40 +0530

    2nd commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname +'index.html');
+  res.sendFile(__dirname +'/index.html');
 });
 
 app.listen(3000, function () {
